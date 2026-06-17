7b289fe10016f33c345d358356df8668ad4c81b0
Rodrigo Arenas, CC5303 SSDD <roarenas@dcc.uchile.cl>
2026-06-16 21:21:04 -0400

    webapp solo atiende a 1 cliente por vez y rechaza a otros

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-#
-# CC5303_lab2
-#
+<h1> CC5303_lab2 </h1>
+En el siguiente laboratorio partiremos con base al __laboratirio 1__, la idea en esta ocacion es llegar a poseer una infraestructura en que tengamos multiples clientes conectados a multiples servidores.
+
+Las distintas etapas del laboratorio estan definidas mediante el _tag_ y una breve descripción de la etapa.
+
+__f6b25a1__ Inicio del laboratorio
+__616cf2c__ La app client espera a que el servicio webapp este arriba correctamente. Ademas client deja registro en /tmp/log/log.txt
--- webapp/lab1.py
+++ webapp/lab1.py
@@ -1,25 +1,39 @@
 from flask import Flask, request, jsonify
-import random
 import os
+import time
+import random
+import threading
 
 app = Flask(__name__)
+processing_lock = threading.Lock()
 
 @app.route("/random", methods=["GET"])
 def random_number():
-    # Read seed from query parameter
-    seed = request.args.get("seed", type=int)
+    if not processing_lock.acquire(blocking=False):
+        return jsonify({
+            "status": "rejected",
+            "message": "System busy"
+            }), 429
+    try:
+        # Read seed from query parameter
+        seed = request.args.get("seed", type=int)
 
-    # Create RNG with seed
-    rng = random.Random(seed)
+        # Create RNG with seed
+        rng = random.Random(seed)
 
-    # Generate random number
-    number = rng.randint(1, 100)
-    #number = int(rng.integers(1, 101))
+        # Generate random number
+        number = rng.randint(1, 100)
+        #number = int(rng.integers(1, 101))
 
-    return jsonify({
-        "seed": seed,
-        "random_number": number
-    })
+        time.sleep(5)
+
+        return jsonify({
+            "status": "ok",
+            "message": number
+        })
+
+    finally:
+        processing_lock.release()
 
 if __name__ == "__main__":
     # Use PORT env variable if available, otherwise default to 5000
